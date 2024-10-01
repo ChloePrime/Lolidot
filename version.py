76241c3647b8dda824cab8311d6c89789d50deb0
ChloePrime <chloe_von_einzbern@qq.com>
2024-10-01 15:26:33 +0800

    Create Official Builds

--- version.py
+++ version.py
@@ -1,3 +1,5 @@
+import os
+
 short_name = "lolidot"
 name = "Lolidot Engine"
 major = 4
@@ -7,3 +9,4 @@ status = "stable"
 module_config = ""
 website = "https://github.com/ChloePrime"
 docs = "4.3"
+os.environ["BUILD_NAME"] = "official"
